48ad0af64208e67a4b0617a80f9dfb508db49997
SoinSoin <vincent.retore@gmail.com>
2018-02-28 16:36:39 +0100

    fin

--- dist/js/app.js
+++ dist/js/app.js
@@ -17,27 +17,30 @@ $(document).ready(function () {
                 type: "Get",
                 dataType: "jsonp",
                 success: function (data) {
+                    console.log(data);
                     var toto = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=fr&APPID=1589f92cc7f59b3d3132a9cff49a2286";
-                    
+                    var long = data.coord.lon;
+                    var lat = data.coord.lat;
+                  
                     window.fetch(toto)
 
                         .then(reponse => reponse.json())
 
-                        .then(reponseJson => $("#temp").text(reponseJson.main.temp+"°"))
-
-                    window.fetch(toto)
-
-                        .then(reponse => reponse.json())
-
-                        .then(reponseJson => $("#tmax").text(reponseJson.main.temp_max+"°"))
-
-
+                        .then(reponseJson => $("#temp").text(parseInt(reponseJson.main.temp) + "°"))
 
                     window.fetch(toto)
 
                         .then(reponse => reponse.json())
 
-                        .then(reponseJson => $("#tmin").text(reponseJson.main.temp_min+"°"))
+                        .then(reponseJson => $("#tmax").text(reponseJson.main.temp_max + "°"))
+
+
+
+                    window.fetch(toto)
+
+                        .then(reponse => reponse.json())
+
+                        .then(reponseJson => $("#tmin").text(reponseJson.main.temp_min + "°"))
 
                     window.fetch(toto)
 
@@ -49,19 +52,19 @@ $(document).ready(function () {
 
                         .then(reponse => reponse.json())
 
-                        .then(reponseJson => $("#vdt").text(reponseJson.wind.speed+"km/h"))
+                        .then(reponseJson => $("#vdt").text(reponseJson.wind.speed + "km/h"))
 
                     window.fetch(toto)
 
                         .then(reponse => reponse.json())
 
-                        .then(reponseJson => $("#humid").text(reponseJson.main.humidity+"%"))
+                        .then(reponseJson => $("#humid").text(reponseJson.main.humidity + "%"))
 
                     window.fetch(toto)
 
                         .then(reponse => reponse.json())
 
-                        .then(reponseJson => $("#longitude").text(reponseJson.coord.lon))
+                        .then(reponseJson => $("#ilong,#rlong").text(reponseJson.coord.lon))
 
 
 
@@ -69,11 +72,16 @@ $(document).ready(function () {
 
                         .then(reponse => reponse.json())
 
-                        .then(reponseJson => $("#latitude").text(reponseJson.coord.lat))
-                        
+                        .then(reponseJson => $("#ilat,#rlat").text(reponseJson.coord.lat))
+
+                        var gmap ="<iframe src='https://www.google.com/maps/embed/v1/place?key=AIzaSyD-IlL_WgfCBymed2DbYWTivev3WaBHhZQ&q="+lat+","+long +"&zoom=16 &maptype=roadmap' width='100%' height='100%' frameborder='0'></iframe>";
+                        console.log(gmap);
+                        $("#imap,#rmap").html(gmap);
+
                 }
-                
+
             });
+
         }
 
 
