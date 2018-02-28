69c122630573dceda484fb4ef750ff133734ce8f
SoinSoin <vincent.retore@gmail.com>
2018-02-28 17:14:24 +0100

    correction certnaine de bug

--- dist/js/app.js
+++ dist/js/app.js
@@ -9,19 +9,19 @@ $(document).ready(function () {
     $("#date").text(moment().format('LL'));
 
 
-    $(".waves-light").click(function () {
+    $(".waves-light").on("click",function () {
         var city = $("#icon_prefix").val();
         if (city != ' ') {
             $.ajax({
                 url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&APPID=1589f92cc7f59b3d3132a9cff49a2286",
                 type: "Get",
-                dataType: "jsonp",
+                dataType: "json",
                 success: function (data) {
                     console.log(data);
                     var toto = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=fr&APPID=1589f92cc7f59b3d3132a9cff49a2286";
                     var long = data.coord.lon;
                     var lat = data.coord.lat;
-                  
+
                     window.fetch(toto)
 
                         .then(reponse => reponse.json())
@@ -74,9 +74,9 @@ $(document).ready(function () {
 
                         .then(reponseJson => $("#ilat,#rlat").text(reponseJson.coord.lat))
 
-                        var gmap ="<iframe src='https://www.google.com/maps/embed/v1/place?key=AIzaSyD-IlL_WgfCBymed2DbYWTivev3WaBHhZQ&q="+lat+","+long +"&zoom=16 &maptype=roadmap' width='100%' height='100%' frameborder='0'></iframe>";
-                        console.log(gmap);
-                        $("#imap,#rmap").html(gmap);
+                    var gmap = "<iframe src='https://www.google.com/maps/embed/v1/place?key=AIzaSyD-IlL_WgfCBymed2DbYWTivev3WaBHhZQ&q=" + lat + "," + long + "&zoom=16 &maptype=roadmap' width='100%' height='100%' frameborder='0'></iframe>";
+                    console.log(gmap);
+                    $("#imap,#rmap").html(gmap);
 
                 }
 
